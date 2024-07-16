8457d22127ed334b6c4ac460abcbc998e42a76c6
Chidananda <chidanandakm99@gmail.com>
2024-07-16 23:13:06 +0530

    ft : landing screen design

---
+++ components/CustomFormFields.tsx
@@ -0,0 +1,98 @@
+"use client";
+import React from "react";
+import { Control } from "react-hook-form";
+
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { FormFieldTypes } from "./forms/PatientForm";
+import Image from "next/image";
+
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import { E164Number } from "libphonenumber-js";
+interface CustomProps {
+  control: Control<any>;
+  fieldType: FormFieldTypes;
+  name: string;
+  label?: string;
+  placeholder?: string;
+  iconSrc?: string;
+  iconAlt?: string;
+  disabled?: boolean;
+  dateFormat?: string;
+  showTimeSelect?: boolean;
+  children?: React.ReactNode;
+  renderSkeleton?: (field: any) => React.ReactNode;
+}
+
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+  const { fieldType, iconSrc, iconAlt, placeholder, showTimeSelect } = props;
+  switch (fieldType) {
+    case FormFieldTypes.INPUT:
+      return (
+        <div className="flex rounded-md border border-dark-500 bg-dark-400">
+          {iconSrc && (
+            <Image
+              src={iconSrc}
+              height={24}
+              width={24}
+              alt={iconAlt || "icon"}
+              className="ml-2"
+            />
+          )}
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              className="shad-input border-0"
+              {...field}
+            />
+          </FormControl>
+        </div>
+      );
+    case FormFieldTypes.PHONE_INPUT:
+      return (
+        <FormControl>
+          <PhoneInput
+            defaultCountry="IN"
+            international
+            withCountryCallingCode
+            value={field.value as E164Number | undefined}
+            onChange={field.onChange}
+            className="input-phone"
+            placeholder={placeholder}
+          />
+        </FormControl>
+      );
+    default:
+      break;
+  }
+  // return <Input type="text" {...field} placeholder="shadcn" />;
+};
+
+const CustomFormFields = (props: CustomProps) => {
+  const { control, fieldType, name, label } = props;
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex-1">
+          {fieldType !== FormFieldTypes.CHECKBOX && label && (
+            <FormLabel>{label}</FormLabel>
+          )}
+          <RenderField field={field} props={props} />
+          <FormMessage className="shad-error" />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormFields;
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,7 +1,91 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Form } from "@/components/ui/form";
+import CustomFormFields from "../CustomFormFields";
+import SubmitButton from "../SubmitButton";
+import { UserFormSchema } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+
+export enum FormFieldTypes {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  CHECKBOX = "checkbox",
+  PHONE_INPUT = "phone-input",
+  DATE_PICKER = "date-picker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
 
 const PatientForm = () => {
-  return <div>PatientForm</div>;
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof UserFormSchema>>({
+    resolver: zodResolver(UserFormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit({ name, email, phone }: z.infer<typeof UserFormSchema>) {
+    setIsLoading(true);
+
+    try {
+      // const userData = { name, email, phone };
+      // const user = await createUser(userData);
+      // if (user) {
+      //   router.push(`/patient/${user.$id}/register`);
+      // }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1>Hi There 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment</p>
+        </section>
+        <CustomFormFields
+          control={form.control}
+          fieldType={FormFieldTypes.INPUT}
+          name="name"
+          label="Full Name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+        <CustomFormFields
+          control={form.control}
+          fieldType={FormFieldTypes.INPUT}
+          name="email"
+          label="Email"
+          placeholder="JohnDoe@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+        <CustomFormFields
+          control={form.control}
+          fieldType={FormFieldTypes.PHONE_INPUT}
+          name="phone"
+          label="Phone Number"
+          placeholder="5551231231"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
 };
 
 export default PatientForm;
